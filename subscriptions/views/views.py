15046b5ea4862514b9bc736e4053f001e8524264
00riddle00 <tomasgiedraitis@gmail.com>
2016-09-09 11:46:27 +0000

    Fix views.py

--- subscriptions/views/views.py
+++ subscriptions/views/views.py
@@ -1,31 +1,45 @@
-from pyramid.response import Response
-from pyramid.view import view_config
-
+from email_validator import validate_email, EmailNotValidError
 from sqlalchemy.exc import DBAPIError
 
-from ..models import Subscriber, Category
+from pyramid.response import Response
+from pyramid.httpexceptions import HTTPFound
 
-from email_validator import validate_email, EmailNotValidError
+from pyramid.security import (
+    remember,
+    forget,
+    )
+
+from pyramid.view import (
+    view_config,
+    view_defaults,
+    forbidden_view_config,
+    )
+
+from ..models import Subscriber, Category
+from ..security import (
+    USERS,
+    check_password,
+)
 
 @view_config(route_name='register_view', renderer='../templates/register.jinja2')
 def register_view(request):
-    
-    categories = request.dbsession.query(Category).all()
-    return {'categories': categories, 'errors':False, 'success':False}
+
+    categories = request.dbsession.query(Category)
+    return {'categories': categories, 'errors':False, 'success':True}
 
 @view_config(route_name='register_received_view', renderer='../templates/register.jinja2')
 def register_received_view(request):
-    
-    categories = request.dbsession.query(Category).all() 
+
+    categories = request.dbsession.query(Category).all()
 
     try:
         name = request.params['name']
-        email = request.params['email'] 
-        categories_chosen = request.params.getall('categories') 
+        email = request.params['email']
+        categories_chosen = request.params.getall('categories')
     except KeyError:
         return {'categories': categories, 'errors':False, 'success':False}
 
-    #validate inputs
+    # validate inputs
     errors = {}
     if len(name)<1:
         errors['name']="You must provide a name longer than 1 character"
@@ -37,7 +51,7 @@ def register_received_view(request):
         email = v["email"] # replace with normalized form
     except EmailNotValidError as e:
         # email is not valid, exception message is human-readable
-        errors['email']="Enter a valid email" 
+        errors['email']="Enter a valid email"
 
     if errors!={}:
         # show the errors and retain the inputs
@@ -45,44 +59,89 @@ def register_received_view(request):
     else:
         # if inputs correct, try to save subscription and load a fresh form
 
-        try: 
-            new_subscriber = Subscriber(name=name, email=email) 
+        try:
+            new_subscriber = Subscriber(name=name, email=email)
             for cat in categories_chosen:
-                query = request.dbsession.query(Category) 
-                category = query.filter(Category.name == cat).first() 
+                query = request.dbsession.query(Category)
+                category = query.filter(Category.name == cat).first()
                 new_subscriber.categories.append(category)
             request.dbsession.add(new_subscriber)
         except DBAPIError:
-
-            return Response(db_err_msg, content_type='text/plain', status=500)
+            return Response("Database error. We are instantly notified and  will fix it soon!", content_type='text/plain', status=500)
         return {'categories': categories, 'errors':False, 'success':True}
 
 
-@view_config(route_name='list_view', renderer='../templates/list.jinja2')
-def list_view(request):
-    
-    subscriptions = request.dbsession.query(Subscriber).all()
-    return {'subscriptions': subscriptions, 'errors':False, 'success':False}
+
+@view_defaults(renderer='../templates/home.pt')
+class UserViews:
+    def __init__(self, request):
+        self.request = request
+        self.logged_in = request.authenticated_userid
+
+    @view_config(route_name='login', renderer='../templates/login.pt')
+    @forbidden_view_config(renderer='../templates/login.pt')
+    def login(self):
+        request = self.request
+
+        login_url = request.route_url('login')
+        referrer = request.url
+        if referrer == login_url:
+            referrer = '/'  # never use login form itself as came_from
+        came_from = request.params.get('came_from', referrer)
+        message = ''
+        login = ''
+        password = ''
+        if 'form.submitted' in request.params:
+            login = request.params['login']
+            password = request.params['password']
 
 
+            if check_password(password, USERS.get(login)):
+                headers = remember(request, login)
+                return HTTPFound(location=came_from,
+                                 headers=headers)
+            message = 'Failed login'
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    return {'one': 'one', 'project': 'subscriptions'}
+        return dict(
+            name='Login',
+            message=message,
+            url=request.application_url + '/login',
+            came_from=came_from,
+            login=login,
+            password=password,
+        )
+
+    @view_config(route_name='logout')
+    def logout(self):
+        request = self.request
+        headers = forget(request)
+        url = request.route_url('home')
+        return HTTPFound(location=url,
+                         headers=headers)
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+    @view_config(route_name='list_view_unordered', renderer='../templates/list.jinja2', permission='edit')
+    def list_view_unordered(request):
+        subscriptions = request.dbsession.query(Subscriber).all()
+        return {'subscriptions': subscriptions, 'errors':False, 'success':False}
 
-1.  You may need to run the "initialize_subscriptions_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
+    @view_config(route_name='list_view', renderer='../templates/list.jinja2', permission='edit')
+    def list_view(request):
+        orderBy = request.matchdict['orderBy']
+        if orderBy=='date':
+            subscriptions = request.dbsession.query(Subscriber).order_by("registered desc").all()
+        elif orderBy=='email':
+            subscriptions = request.dbsession.query(Subscriber).order_by("email").all()
+        else:
+            subscriptions = request.dbsession.query(Subscriber).order_by("name").all()
+        return {'subscriptions': subscriptions, 'errors':False, 'success':False, 'orderBy':orderBy}
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+    @view_config(route_name='delete', renderer='../templates/list.jinja2', permission='edit')
+    def delete(request):
+        id_delete = request.matchdict['id']
+        query = request.dbsession.query(Category)
+        sub = query.filter(Subscriber.id == id_delete).first()
+        request.dbsession.delete(sub)
+        subscriptions = request.dbsession.query(Subscriber).order_by("name").all()
+        return HTTPFound(location='/list')
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
