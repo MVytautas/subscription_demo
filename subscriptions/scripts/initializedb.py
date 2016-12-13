c5d7084a79c1d8b58828c8bd7a2680cacbfb7716
Tomas Giedraitis <tomas.g@osos.lt>
2016-12-13 09:45:45 +0200

    Comment initdb script

--- subscriptions/scripts/initializedb.py
+++ subscriptions/scripts/initializedb.py
@@ -28,59 +28,60 @@ def usage(argv):
 
 
 def main(argv=sys.argv):
-    if len(argv) < 2:
-        usage(argv)
-    config_uri = argv[1]
-    options = parse_vars(argv[2:])
-    setup_logging(config_uri)
-    settings = get_appsettings(config_uri, options=options)
-
-    engine = get_engine(settings)
-    Base.metadata.create_all(engine)
-
-    session_factory = get_session_factory(engine)
-
-    with transaction.manager:
-        dbsession = get_tm_session(session_factory, transaction.manager)
-
-        editor = User(name='editor', role='editor')
-        editor.set_password('editor')
-        dbsession.add(editor)
-
-        categories = [
-            'Politics',
-            'Celebrities',
-            'Sports',
-            'Economy',
-            'Entertainment',
-        ]
-
-        for category in categories:
-            entry = Category(name=category)
-            dbsession.add(entry)
-
-        # for development
-
-        for i in range(5):
-
-            name = 'Test Name {}'.format(i)
-            email = 'test.email{}@test.com'.format(i)
-
-            new_user = Subscriber(name=name, email=email)
-
-            # number of categories
-            cat_num = random.randint(1, 5)
-
-            # categories indexes
-            cat_ind = random.sample(range(0, 5), cat_num)
-
-            chosen_categories = []
-            for index in cat_ind:
-                chosen_categories.append(categories[index])
-
-            for cat in chosen_categories:
-                query = dbsession.query(Category)
-                category = query.filter(Category.name == cat).first()
-                new_user.categories.append(category)
-
-                dbsession.add(new_user)
+    pass
+    # if len(argv) < 2:
+    #     usage(argv)
+    # config_uri = argv[1]
+    # options = parse_vars(argv[2:])
+    # setup_logging(config_uri)
+    # settings = get_appsettings(config_uri, options=options)
+    #
+    # engine = get_engine(settings)
+    # Base.metadata.create_all(engine)
+    #
+    # session_factory = get_session_factory(engine)
+    #
+    # with transaction.manager:
+    #     dbsession = get_tm_session(session_factory, transaction.manager)
+    #
+    #     editor = User(name='editor', role='editor')
+    #     editor.set_password('editor')
+    #     dbsession.add(editor)
+    #
+    #     categories = [
+    #         'Politics',
+    #         'Celebrities',
+    #         'Sports',
+    #         'Economy',
+    #         'Entertainment',
+    #     ]
+    #
+    #     for category in categories:
+    #         entry = Category(name=category)
+    #         dbsession.add(entry)
+    #
+    #     # for development
+    #
+    #     for i in range(5):
+    #
+    #         name = 'Test Name {}'.format(i)
+    #         email = 'test.email{}@test.com'.format(i)
+    #
+    #         new_user = Subscriber(name=name, email=email)
+    #
+    #         # number of categories
+    #         cat_num = random.randint(1, 5)
+    #
+    #         # categories indexes
+    #         cat_ind = random.sample(range(0, 5), cat_num)
+    #
+    #         chosen_categories = []
+    #         for index in cat_ind:
+    #             chosen_categories.append(categories[index])
+    #
+    #         for cat in chosen_categories:
+    #             query = dbsession.query(Category)
+    #             category = query.filter(Category.name == cat).first()
+    #             new_user.categories.append(category)
+    #
+    #             dbsession.add(new_user)
